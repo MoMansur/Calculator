8ee777ad91db8fdb7001b3b7295001914109a5c8
Mansur Shehu <mansurshehumsm@gmail.com>
2024-03-30 16:13:08 +0300

    aaa

--- main.js
+++ main.js
@@ -1,122 +1,58 @@
-
 //Varaibles of the Number and Operator Buttons
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 
-
 //Varibles for Clear and Equals (Others)
 const clearBtn = document.getElementById('clear');
-const equals = document.getElementById('equal')
+const equalBtn = document.getElementById('equal')
 
 //Varaibles of the inputBox and Result output
 const firstOutput =  document.getElementById("first-output");
 const resultOutput =  document.getElementById("result-output");
 
-console.log(numberButtons)
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const multiply = document.querySelector(".multiply");
+const divide =document.querySelector(".divide");
 
-//Funtionality Fntio
+//result 
 
-function functionality(){
-
-let inputBoxDisplayer = "";
-let lastBoxDisplayer = 0;
-
-
-
-//Number Display
-numberButtons.forEach(button => {
-  button.addEventListener("click", function(event) {
-    const clickedButton = event.target;  
-     inputBoxDisplayer += clickedButton.textContent;
-
-    if(inputBoxDisplayer === "1"){
-      inputBoxDisplayer = 1;
-    }else  if(inputBoxDisplayer === "2"){
-      inputBoxDisplayer = 2;
-    }else  if(inputBoxDisplayer === "3"){
-      inputBoxDisplayer = 3;
-    }else  if(inputBoxDisplayer === "4"){
-      inputBoxDisplayer = 4;
-    }else  if(inputBoxDisplayer === "5"){
-      inputBoxDisplayer = 5;
-    }else  if(inputBoxDisplayer === "6"){
-      inputBoxDisplayer = 6;
-    }else  if(inputBoxDisplayer === "7"){
-      inputBoxDisplayer = 7;
-    }else  if(inputBoxDisplayer === "8"){
-      inputBoxDisplayer = 8;
-    }else  if(inputBoxDisplayer === "9"){
-      inputBoxDisplayer = 9;
-    }else  if(inputBoxDisplayer === "0"){
-      inputBoxDisplayer = 0;
-    }
-    firstOutput.innerHTML = inputBoxDisplayer;
-  });
-});
-
-//Operator
-let inputOperator = "";
-
-
-operatorButtons.forEach(button => {
-  button.addEventListener("click", function(event) {
-    const clickedButtonOfOperator = event.target;  
-    
-    calc(clickedButtonOfOperator)
-
-    if(clickedButtonOfOperator.textContent === "+"){
-      console.log(addition(inputBoxDisplayer, lastBoxDisplayer))
-    
-    }else if(clickedButtonOfOperator.textContent === "-"){
-      console.log(addition(inputBoxDisplayer, lastBoxDisplayer))
-    
-    }
-  });
-});
-
-//Calc funttion
-function calc(theClickedButton){
-  inputBoxDisplayer += theClickedButton.textContent; 
-  //Display 
-  firstOutput.innerHTML = inputBoxDisplayer;
-}
+//The Main Three
+let firstEntry = "";
+let operatorEntry = "";
+let secondEntry = "";
 
 
 
-//Clear Button
-// clearBtn.addEventListener('click', clear)
-// function clear(){
-//   firstOutput.innerHTML = "";
- 
-// }
-// clear()
-
+class Calculator{
 
 }
-functionality()
-console.log(functionality())
 
 
 
 
 
 
-//Addition
-function addition(a,b){
-  return a + b
-}
-console.log(addition(1,5))
-//subtraction
-function subtract(a,b){
-  let solve = a - b;
-  return solve
-}
-//multiplation
-function multiply(a,b){
-  return a * b;
-}
-//divide
-function divide(a,b){
-  return a / b;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
